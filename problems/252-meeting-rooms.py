316318df5cbeebb27b796f2edd85a3f2418e21dc
y_fukuda <colors92window@gmail.com>
2022-02-24 21:15:35 +0900

    add: 252-meeting-rooms.py

---
+++ problems/252-meeting-rooms.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/meeting-rooms/
+from typing import List
+
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        sorted_intervals = sorted(intervals)
+        for i in range(len(sorted_intervals)-1):
+            if sorted_intervals[i][1] > sorted_intervals[i+1][0]:
+                return False
+        return True
