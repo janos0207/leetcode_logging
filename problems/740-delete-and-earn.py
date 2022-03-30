0f54e56607bbefaa7817a7b73323b4205c5efae4
y_fukuda <colors92window@gmail.com>
2022-03-31 00:42:44 +0900

    add: 740-delete-and-earn.py

---
+++ problems/740-delete-and-earn.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/delete-and-earn/
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        points = defaultdict(int)
+        max_num = 0
+
+        for num in nums:
+            points[num] += num
+            max_num = max(max_num, num)
+
+        max_points = [0] * (max_num + 1)
+        max_points[1] = points[1]
+
+        for i in range(2, len(max_points)):
+            max_points[i] = max(max_points[i-1], max_points[i-2] + points[i])
+
+        return max_points[max_num]
