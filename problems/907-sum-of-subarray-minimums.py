624a85ed5c24aa9de0fd02f7920ff4515b1f6273
y_fukuda <colors92window@gmail.com>
2022-04-02 00:51:20 +0900

    add: 907-sum-of-subarray-minimums.py

---
+++ problems/907-sum-of-subarray-minimums.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/sum-of-subarray-minimums/
+from typing import List
+
+
+class Solution:
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        arr = [0]+arr
+        result = [0] * len(arr)
+        stack = [0]
+        for i in range(len(arr)):
+            while arr[stack[-1]] > arr[i]:
+                stack.pop()
+            j = stack[-1]
+            result[i] = result[j] + (i-j)*arr[i]
+            stack.append(i)
+        return sum(result) % (10**9+7)
