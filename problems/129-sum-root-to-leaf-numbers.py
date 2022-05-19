83e136d4d240820c0b76b4e0e179fe9468e71759
y_fukuda <colors92window@gmail.com>
2022-05-19 23:39:05 +0900

    add: 129-sum-root-to-leaf-numbers.py

---
+++ problems/129-sum-root-to-leaf-numbers.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        res = 0
+
+        def dfs(node, curr):
+            nonlocal res
+            curr = 10 * curr + node.val
+            if not node.left and not node.right:
+                res += curr
+            if node.left:
+                dfs(node.left, curr)
+            if node.right:
+                dfs(node.right, curr)
+
+        dfs(root, 0)
+        return res
