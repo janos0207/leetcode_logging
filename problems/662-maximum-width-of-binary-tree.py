4a2ab3107ae9ef74c2bbc25c17d696993dd8389b
y_fukuda <colors92window@gmail.com>
2022-05-31 21:34:24 +0900

    add: 662-maximum-width-of-binary-tree.py

---
+++ problems/662-maximum-width-of-binary-tree.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/maximum-width-of-binary-tree/
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        self.ans = 0
+        level = [(root, 1)]
+        while level:
+            new_level = []
+            _, max_id = level[0]
+            _, min_id = level[-1]
+            self.ans = max(self.ans, max_id - min_id + 1)
+            for node, idx in level:
+                if node.left:
+                    new_level.append((node.left, 2*idx))
+                if node.right:
+                    new_level.append((node.right, 2*idx-1))
+            level = new_level
+        return self.ans
