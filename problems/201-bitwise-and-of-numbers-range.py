7db2d7d29b3a5af91a3edc2b59528f853ce7c13f
y_fukuda <colors92window@gmail.com>
2022-06-30 22:16:50 +0900

    add: 201-bitwise-and-of-numbers-range.py

---
+++ problems/201-bitwise-and-of-numbers-range.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/bitwise-and-of-numbers-range/
+
+class Solution:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        shift = 0
+        while left != right:
+            left = left >> 1
+            right = right >> 1
+            shift += 1
+        return left << shift
