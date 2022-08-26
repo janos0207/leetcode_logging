d557a0ab1c45ceba6e01c131a790eddd98a9014c
y_fukuda <colors92window@gmail.com>
2022-08-26 22:14:29 +0900

    add: 945-minimum-increment-to-make-array-unique.py

---
+++ problems/945-minimum-increment-to-make-array-unique.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/minimum-increment-to-make-array-unique/
+from typing import List
+
+
+class Solution:
+    def minIncrementForUnique(self, nums: List[int]) -> int:
+        nums.sort()
+        ans = 0
+        for i in range(1, len(nums)):
+            if nums[i] <= nums[i-1]:
+                ans += nums[i-1] - nums[i] + 1
+                nums[i] = nums[i-1] + 1
+        return ans
