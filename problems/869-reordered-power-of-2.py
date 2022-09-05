a54317e4c4a62648caf5db39dcee4232b81783e9
y_fukuda <colors92window@gmail.com>
2022-09-05 21:38:29 +0900

    add: 869-reordered-power-of-2.py

---
+++ problems/869-reordered-power-of-2.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/reordered-power-of-2/
+from collections import Counter
+
+
+class Solution:
+    def reorderedPowerOf2(self, n: int) -> bool:
+        count = Counter(str(n))
+        return any(count == Counter(str(1 << b)) for b in range(31))
