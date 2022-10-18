c86861d715af537fbb3a4e13b104aef9b930b5a4
y_fukuda <colors92window@gmail.com>
2022-10-18 23:48:34 +0900

    add: 735-asteroid-collision.py

---
+++ problems/735-asteroid-collision.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/asteroid-collision/
+from typing import List
+
+
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        for ast in asteroids:
+            while stack and ast < 0 < stack[-1]:
+                if stack[-1] < -ast:
+                    stack.pop()
+                    continue
+                elif stack[-1] == -ast:
+                    stack.pop()
+                    break
+                else:  # stack[-1] > -ast
+                    break
+            else:
+                stack.append(ast)
+
+        return stack
