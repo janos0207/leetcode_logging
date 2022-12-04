fec83fe663154d11cb836de54fd199c7dc06889b
yasu_fukuda <colors92window@gmail.com>
2022-12-04 17:32:52 +0900

    add: 1461-check-if-a-string-contains-all-binary-codes-of-size-k.py

---
+++ problems/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/check-if-a-string-contains-all-binary-codes-of-size-k/
+
+class Solution:
+    def hasAllCodes(self, s: str, k: int) -> bool:
+        total = 1 << k
+        cache = [False] * total
+        all_one = total - 1
+        hash_val = 0
+        
+        for i in range(len(s)):
+            # calculate hash for s[i-k+1:i+1]
+            hash_val = ((hash_val << 1) & all_one) | (int(s[i]))
+            if i >= k-1 and cache[hash_val] == False:
+                cache[hash_val] = True
+                total -= 1
+                if total == 0:
+                    return True
+        return False
