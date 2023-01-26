2168d1a59fc2ed390b2ba26f651a1cfdda18dc60
yasu_fukuda <colors92window@gmail.com>
2023-01-27 08:59:15 +0900

    add: 1302-deepest-leaves-sum.py

---
+++ problems/1302-deepest-leaves-sum.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/deepest-leaves-sum/
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        queue = [root]
+        while queue:
+            s = 0
+            new_queue = []
+            for node in queue:
+                s += node.val
+                if node.left:
+                    new_queue.append(node.left)
+                if node.right:
+                    new_queue.append(node.right)
+            queue = new_queue
+        return s
